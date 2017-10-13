1df9f1750722df53bef45e71ced1c7b44db46e29
Erica-Brock <Eady.Brock@gmail.com>
2017-10-13 18:28:28 -0500

    added styling from inputs

--- scripts.js
+++ scripts.js
@@ -18,9 +18,10 @@ document.addEventListener("DOMContentLoaded", function () {
     //getting all inputs from the document
     var myInputs = document.getElementsByTagName("input");
     var recInfo1 = document.getElementById("rec-height");
-    var recInfo2 = document.getElementById("rec-width").innerHTML;
-    var squareInfo = document.getElementById("square-length").innerHTML;
-    var cirInfo = document.getElementById("circle-radius").innerHTML;
+    var recInfo2 = document.getElementById("rec-width");
+    var squareInfo = document.getElementById("square-length");
+    var cirInfo = document.getElementById("circle-radius");
+    var triInfo = document.getElementById("tri-height");
     //getting all outputs from the document
     var shapeName = document.getElementById("shape-name").innerHTML;
     var width = document.getElementById("width").innerHTML;
@@ -33,11 +34,12 @@ document.addEventListener("DOMContentLoaded", function () {
     //creating a parent class of shape
     var Shape = /** @class */ (function () {
         function Shape(shapeName) {
+            var _this = this;
             this.shapeName = shapeName;
             this.div = document.createElement('div');
-            //div.addEventListener("click", () => {
-            //this click will pull up the shape info once i figure this shit out.
-            //})
+            this.div.addEventListener("dblclick", function () {
+                shapeContainer.removeChild(_this.div);
+            });
         }
         Shape.prototype.draw = function (id) {
             shapeContainer.appendChild(this.div);
@@ -49,7 +51,11 @@ document.addEventListener("DOMContentLoaded", function () {
     var Square = /** @class */ (function (_super) {
         __extends(Square, _super);
         function Square() {
-            return _super.call(this, "Square") || this;
+            var _this = _super.call(this, "Square") || this;
+            var length = squareInfo.value + "px";
+            _this.div.style.width = length;
+            _this.div.style.height = length;
+            return _this;
         }
         return Square;
     }(Shape));
@@ -57,11 +63,15 @@ document.addEventListener("DOMContentLoaded", function () {
         var square = new Square;
         square.draw("square");
     });
-    //creating rectangle class
+    //creating circle class
     var Circle = /** @class */ (function (_super) {
         __extends(Circle, _super);
         function Circle() {
-            return _super.call(this, "Circle") || this;
+            var _this = _super.call(this, "Circle") || this;
+            var radius = cirInfo.value + "px";
+            _this.div.style.width = radius;
+            _this.div.style.height = radius;
+            return _this;
         }
         return Circle;
     }(Shape));
@@ -73,24 +83,38 @@ document.addEventListener("DOMContentLoaded", function () {
     var Rectangle = /** @class */ (function (_super) {
         __extends(Rectangle, _super);
         function Rectangle() {
-            return _super.call(this, "Rectangle") || this;
+            var _this = _super.call(this, "Rectangle") || this;
+            var recHeight = recInfo1.value + "px";
+            var recWidth = recInfo2.value + "px";
+            if (recHeight === recWidth) {
+                alert("that's a square dummy! This is a rectangle.");
+            }
+            else {
+                _this.div.style.height = recHeight;
+                _this.div.style.width = recWidth;
+            }
+            return _this;
         }
         return Rectangle;
     }(Shape));
     recBtn.addEventListener("click", function () {
         var rectangle = new Rectangle;
         rectangle.draw("rectangle");
-        //creating a triangle class
-        var Triangle = /** @class */ (function (_super) {
-            __extends(Triangle, _super);
-            function Triangle() {
-                return _super.call(this, "Triangle") || this;
-            }
-            return Triangle;
-        }(Shape));
-        triBtn.addEventListener("click", function () {
-            var tri = new Triangle;
-            tri.draw("triangle");
-        });
+    });
+    //creating a triangle class
+    var Triangle = /** @class */ (function (_super) {
+        __extends(Triangle, _super);
+        function Triangle() {
+            var _this = _super.call(this, "Triangle") || this;
+            var triHeight = triInfo.value + "px";
+            _this.div.style.borderTop = triHeight + " solid $triangle-color";
+            _this.div.style.borderLeft = triHeight + " solid transparent";
+            return _this;
+        }
+        return Triangle;
+    }(Shape));
+    triBtn.addEventListener("click", function () {
+        var tri = new Triangle;
+        tri.draw("triangle");
     });
 });
--- scripts.ts
+++ scripts.ts
@@ -7,10 +7,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const triBtn: HTMLElement = document.getElementById("triangle-button");
     //getting all inputs from the document
     const myInputs: NodeListOf<HTMLInputElement> = document.getElementsByTagName("input");
-    const recInfo1: HTMLElement = document.getElementById("rec-height");
-    const recInfo2: string = document.getElementById("rec-width").innerHTML;
-    const squareInfo: string = document.getElementById("square-length").innerHTML;
-    const cirInfo: string = document.getElementById("circle-radius").innerHTML;
+    const recInfo1: HTMLInputElement = (<HTMLInputElement>document.getElementById("rec-height"));
+    const recInfo2: HTMLInputElement = (<HTMLInputElement>document.getElementById("rec-width"))
+    const squareInfo: HTMLInputElement = (<HTMLInputElement>document.getElementById("square-length"))
+    const cirInfo: HTMLInputElement = (<HTMLInputElement>document.getElementById("circle-radius"))
+    const triInfo: HTMLInputElement = (<HTMLInputElement>document.getElementById("tri-height"))
     //getting all outputs from the document
     const shapeName: string = document.getElementById("shape-name").innerHTML;
     const width: string = document.getElementById("width").innerHTML;
@@ -38,9 +39,9 @@ document.addEventListener("DOMContentLoaded", function () {
         constructor(shapeName) {
             this.shapeName= shapeName;
             this.div = document.createElement('div');
-            //div.addEventListener("click", () => {
-            //this click will pull up the shape info once i figure this shit out.
-            //})
+            this.div.addEventListener("dblclick", () => {
+                shapeContainer.removeChild(this.div)
+            })
         }
         draw(id):void{ 
             shapeContainer.appendChild(this.div);
@@ -51,18 +52,23 @@ document.addEventListener("DOMContentLoaded", function () {
      class Square extends Shape{
          constructor(){
              super("Square");
-         }
-        
+             let length=`${squareInfo.value}px`;
+             this.div.style.width=length;
+             this.div.style.height=length;
+         }      
      }  
      squareBtn.addEventListener("click", ()=>{
          let square=new Square;
          square.draw("square");
          
      })     
-       //creating rectangle class
+       //creating circle class
         class Circle extends Shape{
             constructor(){
-                super("Circle");  
+                super("Circle"); 
+                let radius=`${cirInfo.value}px`; 
+                this.div.style.width=radius;
+                this.div.style.height=radius;
             }   
         }
         cirBtn.addEventListener("click", ()=>{
@@ -70,23 +76,35 @@ document.addEventListener("DOMContentLoaded", function () {
             circle.draw("circle");
         })
         //creating Rectangle class
-        class Rectangle extends Shape{
-            height:recInfo1.innerHTML
+        class Rectangle extends Shape{            
             constructor(){
                 super("Rectangle")
-               
+                let recHeight=`${recInfo1.value}px`;
+                let recWidth=`${recInfo2.value}px`;
+                if(recHeight===recWidth){
+                    alert("that's a square dummy! This is a rectangle.");
+                }
+                else{
+                this.div.style.height=recHeight;
+                this.div.style.width=recWidth;
+                }
             }
         }
         recBtn.addEventListener("click", ()=>{
             let rectangle=new Rectangle;
             rectangle.draw("rectangle");
+        })
         //creating a triangle class
         class Triangle extends Shape{
             constructor(){
                 super("Triangle");
+                let triHeight=`${triInfo.value}px`;
+                this.div.style.borderTop= `${triHeight} solid $triangle-color`;
+                this.div.style.borderLeft=`${triHeight} solid transparent`;
             }   
         }
         triBtn.addEventListener("click", ()=>{
             let tri=new Triangle;
             tri.draw("triangle");
+        })
 })
