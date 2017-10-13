4c3ab59c49cf8a38dadd84b2f7eb4178adf746e5
Erica-Brock <Eady.Brock@gmail.com>
2017-10-13 15:31:19 -0500

    addded function to shape insert buttons

--- scripts.js
+++ scripts.js
@@ -1,11 +1,23 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 //getting all buttons from the document
 document.addEventListener("DOMContentLoaded", function () {
+    var myButtons = document.getElementsByTagName('button');
     var recBtn = document.getElementById("rectangle-button");
     var squareBtn = document.getElementById("square-button");
     var cirBtn = document.getElementById("circle-button");
     var triBtn = document.getElementById("triangle-button");
     //getting all inputs from the document
-    var recInfo1 = document.getElementById("rec-height").innerHTML;
+    var myInputs = document.getElementsByTagName("input");
+    var recInfo1 = document.getElementById("rec-height");
     var recInfo2 = document.getElementById("rec-width").innerHTML;
     var squareInfo = document.getElementById("square-length").innerHTML;
     var cirInfo = document.getElementById("circle-radius").innerHTML;
@@ -16,19 +28,69 @@ document.addEventListener("DOMContentLoaded", function () {
     var radius = document.getElementById("radius").innerHTML;
     var area = document.getElementById("area").innerHTML;
     var parameter = document.getElementById("parameter").innerHTML;
+    //getting shape container from the document
+    var shapeContainer = document.getElementById("shape-container");
     //creating a parent class of shape
     var Shape = /** @class */ (function () {
-        function Shape() {
-            this.div = HTMLDivElement;
-            var div = document.createElement('div');
-            div.addEventListener("click", function () {
-            });
+        function Shape(shapeName) {
+            this.shapeName = shapeName;
+            this.div = document.createElement('div');
+            //div.addEventListener("click", () => {
+            //this click will pull up the shape info once i figure this shit out.
+            //})
         }
+        Shape.prototype.draw = function (id) {
+            shapeContainer.appendChild(this.div);
+            this.div.id = id;
+        };
         return Shape;
     }());
+    //creating square class
+    var Square = /** @class */ (function (_super) {
+        __extends(Square, _super);
+        function Square() {
+            return _super.call(this, "Square") || this;
+        }
+        return Square;
+    }(Shape));
+    squareBtn.addEventListener("click", function () {
+        var square = new Square;
+        square.draw("square");
+    });
+    //creating rectangle class
+    var Circle = /** @class */ (function (_super) {
+        __extends(Circle, _super);
+        function Circle() {
+            return _super.call(this, "Circle") || this;
+        }
+        return Circle;
+    }(Shape));
+    cirBtn.addEventListener("click", function () {
+        var circle = new Circle;
+        circle.draw("circle");
+    });
+    //creating Rectangle class
+    var Rectangle = /** @class */ (function (_super) {
+        __extends(Rectangle, _super);
+        function Rectangle() {
+            return _super.call(this, "Rectangle") || this;
+        }
+        return Rectangle;
+    }(Shape));
+    recBtn.addEventListener("click", function () {
+        var rectangle = new Rectangle;
+        rectangle.draw("rectangle");
+        //creating a triangle class
+        var Triangle = /** @class */ (function (_super) {
+            __extends(Triangle, _super);
+            function Triangle() {
+                return _super.call(this, "Triangle") || this;
+            }
+            return Triangle;
+        }(Shape));
+        triBtn.addEventListener("click", function () {
+            var tri = new Triangle;
+            tri.draw("triangle");
+        });
+    });
 });
-function getThatShit() {
-}
-/*function get(id){
-    document.getElementById(id);
-}*/ 
--- scripts.ts
+++ scripts.ts
@@ -1,44 +1,92 @@
 //getting all buttons from the document
-document.addEventListener("DOMContentLoaded", function(){
-const recBtn: HTMLElement= document.getElementById("rectangle-button");
-const squareBtn: HTMLElement= document.getElementById("square-button");
-const cirBtn: HTMLElement= document.getElementById("circle-button");
-const triBtn: HTMLElement= document.getElementById("triangle-button");
-//getting all inputs from the document
-const recInfo1:string=document.getElementById("rec-height").innerHTML;
-const recInfo2:string=document.getElementById("rec-width").innerHTML;
-const squareInfo:string=document.getElementById("square-length").innerHTML;
-const cirInfo:string=document.getElementById("circle-radius").innerHTML;
-//getting all outputs from the document
-const shapeName:string=document.getElementById("shape-name").innerHTML;
-const width:string=document.getElementById("width").innerHTML;
-const height:string=document.getElementById("height").innerHTML;
-const radius:string=document.getElementById("radius").innerHTML;
-const area:string=document.getElementById("area").innerHTML;
-const parameter:string=document.getElementById("parameter").innerHTML;
-//creating the interface for a shape object
-interface IShape{
-    shapeName:string;
-    width?:number;
-    height?:number;
-    radius?:number;
-    area?:number;
-    parameter?:number;
-}
-//creating a parent class of shape
-class Shape{
-    div=HTMLDivElement
-    constructor(){
-        let div=document.createElement('div');
-        div.addEventListener("click", ()=>{
-
-        })
+document.addEventListener("DOMContentLoaded", function () {
+    const myButtons: NodeListOf<HTMLButtonElement> = document.getElementsByTagName('button');
+    const recBtn: HTMLElement = document.getElementById("rectangle-button");
+    const squareBtn: HTMLElement = document.getElementById("square-button");
+    const cirBtn: HTMLElement = document.getElementById("circle-button");
+    const triBtn: HTMLElement = document.getElementById("triangle-button");
+    //getting all inputs from the document
+    const myInputs: NodeListOf<HTMLInputElement> = document.getElementsByTagName("input");
+    const recInfo1: HTMLElement = document.getElementById("rec-height");
+    const recInfo2: string = document.getElementById("rec-width").innerHTML;
+    const squareInfo: string = document.getElementById("square-length").innerHTML;
+    const cirInfo: string = document.getElementById("circle-radius").innerHTML;
+    //getting all outputs from the document
+    const shapeName: string = document.getElementById("shape-name").innerHTML;
+    const width: string = document.getElementById("width").innerHTML;
+    const height: string = document.getElementById("height").innerHTML;
+    const radius: string = document.getElementById("radius").innerHTML;
+    const area: string = document.getElementById("area").innerHTML;
+    const parameter: string = document.getElementById("parameter").innerHTML;
+    //getting shape container from the document
+    const shapeContainer:HTMLElement=document.getElementById("shape-container");
+    //creating the interface for a shape object
+    interface IShape {
+        shapeName: string;
+        width?: number;
+        height?: number;
+        radius?: number;
+        area?: number;
+        parameter?: number;
+        draw(id):void;
     }
-}
+    //creating a parent class of shape
+    class Shape implements IShape {
+        shapeName:string;
+        div:HTMLDivElement;
+        id:string;
+        constructor(shapeName) {
+            this.shapeName= shapeName;
+            this.div = document.createElement('div');
+            //div.addEventListener("click", () => {
+            //this click will pull up the shape info once i figure this shit out.
+            //})
+        }
+        draw(id):void{ 
+            shapeContainer.appendChild(this.div);
+            this.div.id=id;  
+        }
+    }
+     //creating square class
+     class Square extends Shape{
+         constructor(){
+             super("Square");
+         }
+        
+     }  
+     squareBtn.addEventListener("click", ()=>{
+         let square=new Square;
+         square.draw("square");
+         
+     })     
+       //creating rectangle class
+        class Circle extends Shape{
+            constructor(){
+                super("Circle");  
+            }   
+        }
+        cirBtn.addEventListener("click", ()=>{
+            let circle=new Circle;
+            circle.draw("circle");
+        })
+        //creating Rectangle class
+        class Rectangle extends Shape{
+            height:recInfo1.innerHTML
+            constructor(){
+                super("Rectangle")
+               
+            }
+        }
+        recBtn.addEventListener("click", ()=>{
+            let rectangle=new Rectangle;
+            rectangle.draw("rectangle");
+        //creating a triangle class
+        class Triangle extends Shape{
+            constructor(){
+                super("Triangle");
+            }   
+        }
+        triBtn.addEventListener("click", ()=>{
+            let tri=new Triangle;
+            tri.draw("triangle");
 })
-function getThatShit(){
-
-}
-/*function get(id){
-    document.getElementById(id);
-}*/
